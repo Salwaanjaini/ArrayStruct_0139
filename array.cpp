d1e1b6356e1d06070a0b0e5bfdc7ca37fcaf8983
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-04-15 22:51:34 +0700

    menghapus Deklarasi Array

--- array.cpp
+++ array.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 using namespace std;
 
-//deklarasi array tanpa ukuran, wajib diisi data
-int nilai[]={1,2,3};
-//deklarasi array dengan ukuran, diisi data awal
-int skor[4]={12,24,48,64};
-//deklarasi array dengan ukuran, tanpa nilai awal
-string nama[3];
 
 int main() {
     //menampilkan isi array pada index tertentu
