3eed491c8dee359438340be18323c4ea06c0e5b8
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-04-15 22:57:09 +0700

    menghapus isi array pada index tertentu

--- array.cpp
+++ array.cpp
@@ -3,12 +3,7 @@ using namespace std;
 
 
 int main() {
-    //menampilkan isi array pada index tertentu
-    cout << "Tampilkan skor ke 3 = " << skor[2] << endl;
-    //mengisi array pada index tertentu
-    skor[2] = 100; 
-    //menampilkan isi array pada index tertentu
-    cout << "Tampilkan skor ke 3 = " << skor[2] << endl;
+    
 
     //mengisi array dengan loop for
     for(int n = 0 ; n < 3 ; n++ ){
